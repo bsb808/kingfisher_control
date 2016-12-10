f90ea8364cc77ff1c908aa966835a6d898694ec8
Bingham, Brian S <briansbingham@gmail.com>
2016-12-09 16:06:20 -0800

    working on control tuning

--- nodes/kingfisher_yaw_control.py
+++ nodes/kingfisher_yaw_control.py
@@ -9,14 +9,14 @@ from math import pi
 
 # ROS
 import rospy
+import tf
 from dynamic_reconfigure.server import Server
-#from YawDynamic.cfg import YawDynamicConfig #TutorialsConfig
-#from YawDynamicConfig import YawDynamicConfig #TutorialsConfig
 from kingfisher_control.cfg import YawDynamicConfig
 
 from geometry_msgs.msg import Vector3
 from std_msgs.msg import Float64
 from kingfisher_msgs.msg import Drive
+from sensor_msgs.msg import Imu
 
 # BSB
 import pypid
@@ -24,12 +24,16 @@ import pypid
 
 class Node():
     def __init__(self):
-        Kp=0.5
+        Kp=0.0
         Ki=0.0
         Kd=0.1
         self.pid = pypid.Pid(Kp,Ki,Kd)
         self.pid.set_setpoint(-pi/2)
         self.pid.set_inputisangle(True,pi)
+        self.pid.set_derivfeedback(True)  # D term in feedback look
+        fc = 50;  # cutoff freq in hz
+        wc = fc/(2.0*pi)  # cutoff freq. in rad/s
+        self.pid.set_derivfilter(1,wc)
         self.drivemsg = None
         self.publisher = None
         self.lasttime = None
@@ -38,8 +42,13 @@ class Node():
         self.pid.set_setpoint(msg.data)
 
     def callback(self,msg):
-        # Got a RPY
-        yaw = msg.z
+        # Get quaternion from Imu message
+        q = (msg.orientation.x,
+             msg.orientation.y,
+             msg.orientation.z,
+             msg.orientation.w)
+        euler = tf.transformations.euler_from_quaternion(q)
+        yaw = euler[2]
         now = rospy.get_time()
         if self.lasttime is None:
             self.lasttime = now
@@ -52,6 +61,7 @@ class Node():
         self.drivemsg.left=-1*torque
         self.drivemsg.right=torque
         self.publisher.publish(self.drivemsg)
+
     def dynamic_callback(self,config,level):
         #rospy.loginfo("""Reconfigure Request: {int_param}, {double_param},\ 
         #  {str_param}, {bool_param}, {size}""".format(**config))
@@ -64,20 +74,34 @@ class Node():
         return config
         
 
-
-
 if __name__ == '__main__':
     
     rospy.init_node('kingfisher_yaw_pid', anonymous=True)
     
-    # Initiate node object
+    # ROS Parameters
+    in_topic = rospy.get_param('~input_topic','imu')
+    out_topic = rospy.get_param('~output_topic','cmd_drive')
+    Kp = rospy.get_param('~Kp',1.0)
+    Kd = rospy.get_param('~Kd',0.0)
+    Ki = rospy.get_param('~Ki',0.0)
+    
+    # Initiate node object - creates PID object
     node=Node()
     
+    # Set initial gains from parameters
+    node.pid.Kp = Kp
+    node.pid.Kd = Kd
+    node.pid.Ki = Ki
+
     # Setup outbound message
     node.drivemsg = Drive()
 
-    in_topic = "kingfisher_rpy"
-    out_topic = "cmd_drive"
+    #in_topic = "kingfisher_rpy"
+    #in_type = Vector3
+
+    in_type = Imu
+    
+
     # Setup publisher
     rospy.loginfo("Subscribing to %s"%
                   (in_topic))
@@ -86,7 +110,7 @@ if __name__ == '__main__':
     node.publisher = rospy.Publisher(out_topic,Drive,queue_size=10)
 
     # Setup subscribers
-    rospy.Subscriber(in_topic,Vector3,node.callback)
+    rospy.Subscriber(in_topic,in_type,node.callback)
     rospy.Subscriber("set_setpoint",Float64,node.set_setpoint)
     
     # Dynamic configure
